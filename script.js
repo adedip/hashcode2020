fb17d15f48e3a511b6527ec44891126c8abe9e1c
Andrea D'Ippolito <dippolito@webmonks.it>
2020-02-20 19:39:51 +0100

    input structure

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 function solution() {
     var fs = require("fs");
   
-    var lineByLineArray = fs
+    let lines = fs
       .readFileSync("a_example.txt", "utf8")
       .toString()
+      .match(/^.+$/gm);
+    
+    let [books,libraries,days] = lines[0].split(' ')
+    let books_scores = lines[1].split(' ')
+    
+    let data = new Map()
+
+    for(i=0; i < parseInt(libraries); i++){
+      l = i*2
+      console.log("id: "+i+ " l: "+l)
+      let[lib_books, lib_days, lib_ship] = lines[2+l].split(' ')
+      data.set(i, {
+        lib_schedule: {
+          lib_books: lib_books, 
+          lib_days: lib_days, 
+          lib_ship: lib_ship
+        },
+        book_n: lines[2+l+1]
+      })
+    } 
     
     debugger
 }
